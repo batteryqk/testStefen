dee5a01e1997f65b7cfaffee3768e07972de41a6
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-07-07 13:04:47 +0600

    some issue fix

--- resources/views/frontend/pages/detail.blade.php
+++ resources/views/frontend/pages/detail.blade.php
@@ -1,10 +1,10 @@
 <x-frontend::layout>
-    <section class="bg-white px-4 py-20 sm:py-24 md:py-28 lg:py-32 xl:py-40">
-        <div class="container mx-auto max-w-8xl grid grid-cols-1 md:grid-cols-2 gap-10 items-start">
+    <section class="bg-white px-4 py-20 sm:pt-24 md:pt-28 lg:pt-40   mt-4 lg:mt-8 xl:mt-16">
+        <div class="container mx-auto max-w-8xl grid grid-cols-1 md:grid-cols-2 gap-4 items-start">
 
             <!-- Image Slider -->
-            <div class="px-0 sm:px-6 md:px-8 lg:px-10 xl:px-13">
-                <div class="w-full h-full aspect-square overflow-hidden rounded-lg shadow-lg border-t border-gray-200">
+            <div class="px-0 sm:px-6 md:px-8  ">
+                <div class="w-full h-full aspect-square overflow-hidden  shadow-md ">
                     <img id="mainImage" src="{{ $product->primaryImage?->first()?->modified_image }}" alt="Main Product"
                         class="w-full h-full object-cover">
                 </div>
@@ -18,10 +18,9 @@
 
                     <!-- Thumbnails Scroll Container -->
                     <div id="thumbnailScroll" class="flex gap-2 mt-4 overflow-x-auto px-6 scroll-smooth no-scrollbar">
-
                         @foreach ($product->nonPrimayImages as $thumbImage)
                             <img src="{{ $thumbImage->modified_image }}"
-                                class="w-23 sm:w-26 md:w-34 lg:w-32 xl:w-49 h-24 sm:h-28 md:h-32 object-cover cursor-pointer rounded-lg border {{ in_array($thumbImage->modified_image, ['r1.png', 'r2.png', 'r4.png']) ? 'bg-gray-50' : '' }} flex-shrink-0"
+                                class="w-23 sm:w-26 md:w-34 lg:w-32 xl:w-49 h-24 sm:h-28 md:h-36 object-cover cursor-pointer  {{ in_array($thumbImage->modified_image, ['r1.png', 'r2.png', 'r4.png']) ? 'bg-gray-50' : '' }} flex-shrink-0"
                                 onclick="document.getElementById('mainImage').src = this.src">
                         @endforeach
                     </div>
@@ -35,7 +34,7 @@
             </div>
 
             <!-- Product Info -->
-            <div class="px-1 sm:px-3 md:px-6">
+            <div class="px-1 sm:px-3 md:px-6 mt-4">
                 <div class="mb-2 text-sm sm:text-base">
                     <p>Home / <span class="">T-shirt Valgrit</span></p>
                 </div>
@@ -57,7 +56,7 @@
                     <div class="mb-4">
                         <p class="font-semibold mb-2">Size:</p>
                         <select class="border rounded px-7 py-2 w-full sm:w-50">
-                            <option value="" selected disabled hidden>Selecte Size</option>
+                            <option value="" selected disabled hidden>Selecionar Tamanho</option>
                             @foreach ($product->attribute_values as $key => $size)
                                 <option value="{{ $key }}">
                                     {{ $size }}
@@ -101,9 +100,9 @@
         </div>
     </section>
 
-
-    <section class="bg-white mb-8" id="development">
-        <div class="container mx-auto max-w-8xl">
+{{-- related products --}}
+    <section class="bg-white pb-24" id="development">
+        <div class="container mx-auto max-w-[1820px]">
             <div class="relative px-4 mb-8">
                 <div class="text-center px-2 sm:px-0">
                     <h1 class="text-3xl sm:text-5xl md:text-6xl font-bold mb-2 sm:mb-4 text-gray-900">
