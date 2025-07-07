62adc517e5c11e5b2caf1b661715c254da40a93e
Wasif Ahmed <155315416+wasifahmed85@users.noreply.github.com>
2025-07-07 12:10:40 +0600

    details name daynamic

--- resources/views/components/frontend/detail.blade.php
+++ resources/views/components/frontend/detail.blade.php
@@ -1,11 +1,11 @@
  @props(['product'])
 
  <div class="swiper-slide bg-white rounded-xl flex items-center border-2 border-gray-200 p-4 gap-4 h-32 relative">
-     <a href="javascript:void(0)" class="absolute inset-0 bg-transparent"></a>
-     <img src="{{ asset('assets/frontend/imagens/r6.png') }}" alt="24/7 Support" class="w-full h-full">
+     <a href="{{ route('f.detail', $product->slug) }}" class="absolute inset-0 bg-transparent"></a>
+     <img src="{{ $product->primaryImage?->first()?->modified_image }}" alt="24/7 Support" class="w-full h-full">
      <div class="flex flex-col sm:flex-row items-start sm:items-center justify-between mt-4">
-         <h5 class="text-xl font-bold text-gray-800">Casaco Valgrit Azul</h5>
-         <h4 class="text-2xl font-extrabold text-black mt-1 sm:mt-0">45,99€</h4>
+         <h5 class="text-xl font-bold text-gray-800">{{$product->name}}</h5>
+         <h4 class="text-2xl font-extrabold text-black mt-1 sm:mt-0">{{$product->price}}€</h4>
      </div>
 
  </div>
